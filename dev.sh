8ad08f908b3e0202c3dc8acb1a5325895d8124fc
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-03-10 07:21:10 -0300

    configurando database com variável de ambiente

---
+++ dev.sh
@@ -0,0 +1,6 @@
+export DB_DATABASE="desafioqa"
+export DB_USERNAME="root"
+export DB_PASSWORD="root"
+export DB_HOST="localhost"
+
+npm start
---
+++ test.sh
@@ -0,0 +1,6 @@
+export DB_DATABASE="desafioqa_base_qa"
+export DB_USERNAME="root"
+export DB_PASSWORD="root"
+export DB_HOST="localhost"
+
+npm test
