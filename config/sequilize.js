b91ba9d09ec5597e2ed5b02343d8247af124268a
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-03-10 07:21:00 -0300

    configurando database com variável de ambiente

--- config/sequilize.js
+++ config/sequilize.js
@@ -1,7 +1,17 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize('desafioqa', 'root', 'root', {
-    host: 'localhost',
+let database = "desafioqa"
+let usuario = "root"
+let senha = "root"
+let host = "localhost"
+
+if(process.env.DB_DATABASE) database = process.env.DB_DATABASE
+if(process.env.DB_USERNAME) usuario = process.env.DB_USERNAME
+if(process.env.DB_PASSWORD) senha = process.env.DB_PASSWORD
+if(process.env.DB_HOST) host = process.env.DB_HOST
+
+module.exports = new Sequelize(database, usuario, senha, {
+    host: host,
     dialect: 'mysql',
     charset: 'utf8mb4'
 });
